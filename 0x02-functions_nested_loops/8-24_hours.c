2fe6284e78153cff0d6c697d958b363f94b0cf25
WillisXoX <Williamstone4041@gmail.com>
2022-09-13 02:41:30 -0700

    8-24_hours.c complete

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Entry point
+ * Return: Always 0
+ */
+
+void jack_bauer(void)
+{
+	int h = 0, m = 0;
+
+	while (h < 24)
+	{
+		while (m < 60)
+		{
+			_putchar('0' + (h / 10));
+			_putchar('0' + (h % 10));
+			_putchar(':');
+			_putchar('0' + (m / 10));
+			_putchar('0' + (m % 10));
+
+			m++;
+		}
+		m = 0;
+		h++;
+	}
+}
