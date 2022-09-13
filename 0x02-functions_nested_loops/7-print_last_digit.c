09229c4ea313ea3f3156fce1a4e2d15363de5df0
WillisXoX <Williamstone4041@gmail.com>
2022-09-13 10:20:54 -0700

    7-print_last_digit.c complete

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,12 +5,13 @@
  * @n: interger varaible
  * Return: Always 0
  */
+
 int print_last_digit(int n)
 {
 	if (n < 0)
 		n *= -1;
 
-       	_putchar('0' + (n % 10));
+	_putchar('0' + (n % 10));
 
 	return (n % 10);
 }
