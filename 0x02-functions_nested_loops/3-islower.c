e3f969764e102612f4d5646ec447231b2a79dbca
WillisXoX <Williamstone4041@gmail.com>
2022-09-13 01:30:35 -0700

    3-islower.c complete

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * islower - check for lower character
+ * Rreturn: Always 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
