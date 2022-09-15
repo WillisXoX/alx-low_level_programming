1316676cb82aa2e09635abbd6150f3d9b5d6779b
WillisXoX <Williamstone4041@gmail.com>
2022-09-15 01:54:45 -0700

    3-print_numbers.c complete

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ * Return: number from 0 - 9
+ */
+
+void print_numbers(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -30,4 +30,10 @@ int _isdigit(int c);
 
 int mul(int a, int b);
 
+/**
+ * print_numbers -  prints the numbers, from 0 to 9
+ */
+
+void print_numbers(void);
+
 #endif
