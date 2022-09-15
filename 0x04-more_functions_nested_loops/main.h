cb28ed0a3c6ed37790a0b65c2dcfb5bcfec8858d
WillisXoX <Williamstone4041@gmail.com>
2022-09-15 01:25:36 -0700

    0-isupper.c complete

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+* _putchar - puts character to standard output
+* @c: character to put to standard output
+*/
+
+int _putchar(char c);
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: character to put to standard output
+ */
+
+int _isupper(int c);
+
+#endif
