8917dab5b5da3dded9ed2fa2c8a770e5ce883f6a
WillisXoX <Williamstone4041@gmail.com>
2022-09-15 02:06:16 -0700

    4-print_most_numbers.c complete

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -12,6 +12,7 @@ void print_numbers(void)
 	while (c <= '9')
 	{
 		_putchar(c);
+		c++;
 	}
 	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -36,4 +36,10 @@ int mul(int a, int b);
 
 void print_numbers(void);
 
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9 excluding 2 and 4
+ */
+
+void print_most_numbers(void);
+
 #endif
