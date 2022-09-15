77f08deacb96730a07952ab954e814a35ac52481
WillisXoX <Williamstone4041@gmail.com>
2022-09-15 13:20:59 -0700

    0-main.c complete

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/**
+ * postitive_or_negative - tests function that prints if integer is positive or negative
+ * @i: integer variable
+ */
+
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
