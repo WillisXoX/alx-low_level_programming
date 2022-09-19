1de6dc2a1b2558239ab5d32bd23ed8814232bc2f
WillisXoX <Williamstone4041@gmail.com>
2022-09-19 00:19:07 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -15,4 +15,12 @@ int _putchar(char c);
 
 void reset_to_98(int *n);
 
+/**
+ * swap_int - swaps the values of two integers
+ * @a: pointer to an integer
+ * @b: pointer to an integer
+ */
+
+void swap_int(int *a, int *b);
+
 #endif
