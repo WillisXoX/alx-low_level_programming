809016e85d2ca7ec3c30d285e4b9480d57d8b870
WillisXoX <Williamstone4041@gmail.com>
2022-09-19 00:10:04 -0700

    0-reset_to_98.c complete

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - puts character to standard output
+ * @c: character to put to standard output
+ */
+
+int _putchar(char c);
+
+/**
+ * void reset_to_98 - updates the value it points to 98
+ * @n: pointeri to integer
+ */
+
+void reset_to_98(int *n);
+
+#endif
