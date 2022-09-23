a0729a5553ee206932801d7d37f7391bc78fa1d1
WillisXoX <Williamstone4041@gmail.com>
2022-09-23 08:20:07 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -37,4 +37,13 @@ char *_strncat(char *dest, char *src, int n);
 
 char *_strncpy(char *dest, char *src, int n);
 
+/**
+ * _strcmp - compare two strings
+ * @s1: one string
+ * @s2: one string
+ * Return: int that tells num spaces in between
+ */
+
+int _strcmp(char *s1, char *s2);
+
 #endif
