1174ff5d4e75ecbbf9cfe8cbaa7735c4c6ee9714
WillisXoX <Williamstone4041@gmail.com>
2022-09-23 07:27:08 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MIAN_H
+
+/**
+ * _strcat -  function appends the src string to the dest string
+ * @dest: pointer to string
+ * @src: pointer to string
+ */
+
+char *_strcat(char *dest, char *src);
+
+#endif
