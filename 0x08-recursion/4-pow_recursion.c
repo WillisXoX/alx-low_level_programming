ccc16fd5e4814675d3f55dc4a0a3b90c28867f3e
WillisXoX <Williamstone4041@gmail.com>
2022-09-28 12:04:56 -0700

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,33 +1,22 @@
 #include "main.h"
 
 /**
- * find_root - find square root of n, starting from the smallest possible, 0
- * @n: n
- * @root: test this root
- * Return: natural square root, or -1 if not natural root
+ * _pow_recursion - raise x to power y
+ * @x: number
+ * @y: power to raise to
+ * Return: power of a number, -1 if power less than 0
  */
 
-int find_root(int n, int root)
+int _pow_recursion(int x, int y)
 {
-	if (root * root > n)
+	if (y < 0)
 		return (-1);
 
-	if (root * root == n)
-		return (root);
+	if (x == 0)
+		return (0);
 
-	return (find_root(n, root + 1));
-}
+	if (x == 1 || y == 0)
+		return (1);
 
-/**
- * _sqrt_recursion - find natural square root of n
- * @n: n
- * Return: natural square root, or -1 if not natural root
- */
-
-int _sqrt_recursion(int n)
-{
-	if (n < 0)
-		return (-1);
-
-	return (find_root(n, 0));
+	return (x * _pow_recursion(x, y - 1));
 }
